30344df50fd18fd98494da7af227f67ebd49654a
Baoza-J <guantangbaoza@163.com>
2018-10-28 09:58:11 +0800

    wait for row in list table

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
+
+MAX_WAIT =10
 
 class NewVisitorTest(LiveServerTestCase):
     
@@ -12,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() -start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #伊迪丝听说有一个很酷的在线待办事项应用
@@ -38,11 +49,10 @@ class NewVisitorTest(LiveServerTestCase):
         #伊迪丝的爱好是使用假蝇做饵钓鱼
         inputbox.send_keys('Buy peacock feathers') 
 
-        #他按回车键后，页面更新了
+        #她按回车键后，页面更新了
         #待办事项表格中显示了"1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         #页面中又显示了一个文本框，可以输入其他待办事项
         #她输入了"Use peacock feathers to make a fly"(使用孔雀羽毛做饵)
@@ -50,11 +60,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
 
         #页面再次更新，她的清单中显示了这两个待办事项
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         #伊迪丝想知道这个网站是否会记住她的清单
         #她看到网站为她生成了一个唯一的URL
